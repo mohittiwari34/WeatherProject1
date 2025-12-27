e00efca6e04a3a35667fdd64edd867070bd10b03
Mohit Tiwari <mohittiwarigcmb@gmail.com>
2025-12-27 11:30:54 +0530

    advan

--- component/detail.js
+++ component/detail.js
@@ -863,5 +863,5 @@ function getWindDirection(deg) {
 //this is it
 //helo mohit
 //ho are you this moment j k
-//git vanc aur north react node express qustion ert reddis
+//git vanc aur north react node express qustion ert reddis db
 export default Detail;
