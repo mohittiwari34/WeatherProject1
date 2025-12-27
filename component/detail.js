273bc433d44df068638aba78342e390dd593ae97
Mohit Tiwari <mohittiwarigcmb@gmail.com>
2025-12-27 11:35:11 +0530

    adva

--- component/detail.js
+++ component/detail.js
@@ -864,4 +864,5 @@ function getWindDirection(deg) {
 //helo mohit
 //ho are you this moment j k
 //git vanc aur north react node express qustion ert reddis db qes search red qews question socket two 
+//git change
 export default Detail;
