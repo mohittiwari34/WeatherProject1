c67a81422367a6f63086fd508d265bc9504123fe
Mohit Tiwari <mohittiwarigcmb@gmail.com>
2025-12-27 11:29:17 +0530

    adva3ne

--- component/detail.js
+++ component/detail.js
@@ -863,5 +863,5 @@ function getWindDirection(deg) {
 //this is it
 //helo mohit
 //ho are you this moment j k
-//git vanc aur north react node express
+//git vanc aur north react node express qust
 export default Detail;
